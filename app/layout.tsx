f688b7a38ab6f1c37160d9d22a4238248f822356
Ayush9756 <aa7782549@gmail.com>
2025-08-09 18:51:11 +0530

    add  pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner"
+import { usePWA } from "@/hooks/usePWA";
 import {
   ClerkProvider,
   SignInButton,
@@ -41,7 +42,9 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  usePWA();
   return (
+
      <ClerkProvider >
     <html lang="en" suppressHydrationWarning>
       <body
