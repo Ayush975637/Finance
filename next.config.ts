4def39979d0aa643af35b29a52112bb51847ee1b
Ayush9756 <aa7782549@gmail.com>
2025-08-09 18:25:01 +0530

    add pwa featur

--- next.config.ts
+++ next.config.ts
@@ -29,6 +29,8 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  
 });
 
 const nextConfig = {
